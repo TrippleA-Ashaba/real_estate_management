5bc7208206110130b59562360dfc55a0650520c5
TrippleA-Ashaba <trippleaunit@hotmail.com>
2023-09-09 08:31:39 +0300

    Add business and property models

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import (
+    Business,
+    Project,
+    ProjectBudget,
+    ProjectContactPerson,
+    ProjectCustomer,
+    ProjectExpense,
+    ProjectPayments,
+    ProjectSales,
+    ProjectStatus,
+    Admin,
+    Staff,
+)
 
-# Register your models here.
+admin.site.register(Business)
+admin.site.register(Project)
+admin.site.register(ProjectBudget)
+admin.site.register(ProjectContactPerson)
+admin.site.register(ProjectCustomer)
+admin.site.register(ProjectExpense)
+admin.site.register(ProjectPayments)
+admin.site.register(ProjectSales)
+admin.site.register(Admin)
+admin.site.register(Staff)
--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,99 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+
+class Admin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    business = models.ForeignKey(Business, on_delete=models.CASCADE)
+
+
+class Staff(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    business = models.ForeignKey(Business, on_delete=models.CASCADE)
+    can_login = models.BooleanField(default=False)
+
+
+class ProjectStatus(models.TextChoices):
+    NOTSTARTED = "not_started", "not_started"
+    INPROGRESS = "in_progress", "in_progress"
+    CANCELLED = "cancelled", "cancelled"
+    COMPLETED = "completed", "completed"
+    SOLD = "sold", "sold"
+
+
+class Project(models.Model):
+    business = models.ForeignKey(
+        Business, related_name="projects", on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    status = models.CharField(
+        max_length=255, choices=ProjectStatus.choices, default=ProjectStatus.NOTSTARTED
+    )
+
+
+class ProjectContactPerson(models.Model):
+    project = models.ForeignKey(
+        Project, related_name="contact_persons", on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=255)
+    designation = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+
+
+class ProjectBudget(models.Model):
+    project = models.OneToOneField(
+        Project, related_name="budget", on_delete=models.CASCADE
+    )
+    amount = models.PositiveIntegerField()
+
+
+class ProjectExpense(models.Model):
+    project = models.ForeignKey(
+        Project, related_name="expenses", on_delete=models.CASCADE
+    )
+    item = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    quantity = models.PositiveIntegerField()
+    amount = models.PositiveIntegerField()
+
+
+class ProjectCustomer(models.Model):
+    project = models.ForeignKey(
+        Project, related_name="customers", on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    phone = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    nin = models.CharField(max_length=255)
+
+
+class ProjectSales(models.Model):
+    project = models.OneToOneField(
+        Project, related_name="sales", on_delete=models.CASCADE
+    )
+    customer = models.OneToOneField(
+        ProjectCustomer, related_name="customers", on_delete=models.CASCADE
+    )
+    amount = models.PositiveIntegerField()
+    period = models.PositiveIntegerField()
+
+
+class ProjectPayments(models.Model):
+    sales = models.ForeignKey(
+        ProjectSales, related_name="payments", on_delete=models.CASCADE
+    )
+    timestamp = models.DateTimeField(auto_now_add=True)
+    amount = models.PositiveIntegerField()
